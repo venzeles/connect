7c421c6951ac853e6a42b35166f8ecb097e6d5d8
Dmitriy Seleznev <daseleznev@avito.ru>
2019-01-31 01:12:26 +0300

    html + css

--- js/index.js
+++ js/index.js
@@ -8,4 +8,41 @@ document.addEventListener('DOMContentLoaded', function () {
     hide.addEventListener('click', function () {
         menu.style.display = 'none';
     });
+
+    const detailMenu = document.querySelector('.detail_menu');
+    const wrapCol = document.querySelector('.detail_menu_wrap_columns');
+    let press = false;
+    let clientX = 0;
+    let start = 1;
+    detailMenu.addEventListener('click', () => {
+        console.log('click');
+    });
+    detailMenu.addEventListener('mousedown', () => {
+        console.log('mousedown');
+        press = true;
+    });
+    detailMenu.addEventListener('mouseup', () => {
+        console.log('mouseup');
+        press = false;
+    });
+    detailMenu.addEventListener('mousemove', () => {
+        console.log('mousemove');
+        if (press) {
+            console.log(wrapCol);
+        }
+    });
+    detailMenu.addEventListener('touchstart', (e) => {
+        clientX = e.touches[0].clientX;
+        press = true;
+    });
+    detailMenu.addEventListener('touchend', (e) => {
+        clientX = e.changedTouches[0].clientX - clientX;
+        press = false;
+        if (clientX > 0 && clientX !== 0 && start !== 1) {
+            start = start - 1;
+        } else if (clientX < 0 && clientX !== 0 && start !== 5) {
+            start = start + 1;
+        }
+        wrapCol.style.left = `-${204 * (start - 1)}px`;
+    });
 });
